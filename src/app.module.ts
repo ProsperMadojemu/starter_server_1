437c0de3dc8e0cfdd95d3c4917998af71bb8b963
ProsperMadojemu <prospermadojemu00@gmail.com>
2026-05-13 22:46:06 +0100

    feat: add @nestjs/config dependency and configure global settings in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
