75174d13b94ddd80b6566fe8b98deb186b0de65e
ProsperMadojemu <prospermadojemu00@gmail.com>
2026-05-14 08:00:13 +0100

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [UserModule, ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
-  controllers: [AppController, UserController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
